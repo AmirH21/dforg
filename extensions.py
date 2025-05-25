11a05b946746af10d3e26184de67c154fcd2a2d1
AmirH21 <amirhabsari01@gmail.com>
2025-05-25 11:02:09 +0330

    added a file for storing extensions

---
+++ extensions.py
@@ -0,0 +1,21 @@
+COMPRESSED_FORMATS = [
+    '.zip', '.rar', '.7z', '.tar', '.gz',
+    '.bz2', '.xz', '.iso', '.cab', '.dmg', '.z'
+]
+
+VIDEO_FORMATS = [
+    '.mp4', '.mov', '.avi', '.mkv', '.flv',
+    '.wmv', '.webm', '.mpeg', '.mpg', '.3gp',
+    '.m4v', '.ogv', '.ts'
+]
+
+MUSIC_FORMATS = [
+    '.mp3', '.wav', '.flac', '.aac', '.ogg',
+    '.wma', '.m4a', '.aiff', '.alac', '.opus'
+]
+
+DOCUMENT_FORMATS = [
+    '.pdf', '.doc', '.docx', '.txt', '.rtf',
+    '.odt', '.tex', '.md', '.ppt', '.pptx',
+    '.xls', '.xlsx', '.csv', '.epub', '.odp', '.ods'
+]
